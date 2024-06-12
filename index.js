3aee23cc9a29b7545aa3a05d2f5958f8e03d52a8
mafizul247 <mafizul247@gmail.com>
2024-06-12 14:30:06 +0600

    Car Doctor Search

--- index.js
+++ index.js
@@ -52,7 +52,13 @@ async function run() {
 
         app.get('/services', async (req, res) => {
             const sort = req.query.sort;
-            const query = {};
+            const search = req.query.search;
+            let query = {
+                title: {
+                    $regex: search,
+                    $options: 'i'
+                }
+            }
             const options = {
                 sort: { 'price': sort === 'asc' ? 1 : -1 }
             }
